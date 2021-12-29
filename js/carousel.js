90d1f8a330fc4a696c44a1bd233c278dbadd358d
tomohiro.mizukami <water.yukuhashi@gmail.com>
2021-12-29 19:40:50 +0000

    add animation

--- js/carousel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-        autoplay: false, // 自動再生
-        // autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
-        arrows: true, // 左右のナビゲーションを無効にする
-        dots: false, // スライド下にドットを表示する
-        fade: true, // スライドの切り替えをフェードにする        
-    });
-});
---
+++ js/main.js
@@ -0,0 +1,65 @@
+// ハンバーガーメニュー
+$(".openbtn").click(() => { //ボタンがクリックされたら
+  $(".openbtn").toggleClass('active'); //ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
+  $(".circle-bg").toggleClass('circleactive'); //丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a, #header a").click(() => { //ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass('active'); //ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスを除去
+  $(".circle-bg").removeClass('circleactive'); //丸背景のcircleactiveクラスを除去
+});
+
+
+// slick
+$(document).ready(function() {
+  $('.carousel').slick({
+    autoplay: false, // 自動再生
+    // autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+    arrows: true, // 左右のナビゲーションを無効にする
+    dots: false, // スライド下にドットを表示する
+    fade: false, // スライドの切り替えをフェードにする 
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+      }
+    }, ]
+  });
+});
+
+
+// Waypoints
+const effects = ['fadeInUp', 'zoomIn'];
+// ${effect}クラスの付いた要素にwaypointを登録
+effects.forEach((effect) => {
+  // ${effect}クラスの付いた要素にwaypointを登録
+  $(`.${effect}`).waypoint({
+    handler(direction) {
+      // 要素が画面上に来るとここが実行される
+      if (direction === 'down') {
+        /**
+         * 下方向のスクロール
+         * イベント発生元の要素に${effect}クラスを付けることで
+         * アニメーションを開始
+         */
+        $(this.element)
+          .addClass(`animate__${effect}`)
+          .css('opacity', '1')
+          .removeClass(`${effect}`);
+
+        /**
+         * waypointを削除することで、この要素に対しては
+         * これ以降handlerが呼ばれなくなる
+         */
+        this.destroy();
+      }
+    },
+
+    // 要素が画面上に来たらhandlerを実行
+    offset: '100%',
+  });
+});
