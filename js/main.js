f9b6001c38f5dd6227adb6ca52faea5c955dae87
tomohiro.mizukami <water.yukuhashi@gmail.com>
2022-01-03 09:35:02 +0000

    add fullPage.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 });
 
 
-// Waypoints
+// Waypoints + Animate.css
 const effects = ['fadeInUp', 'zoomIn'];
 // ${effect}クラスの付いた要素にwaypointを登録
 effects.forEach((effect) => {
@@ -54,4 +54,40 @@ effects.forEach((effect) => {
     // 要素が画面上に来たらhandlerを実行
     offset: '100%',
   });
+});
+
+
+// ページトップへ戻るボタン
+// ボタンの表示／非表示を切り替える関数
+// const updateButton = () => {
+//   if ($(window).scrollTop() >= 300) {
+//     // 300px以上スクロールされた
+//     // ボタンを表示
+//     $('.back-to-top').fadeIn();
+//   }
+//   else {
+//     // ボタンを非表示
+//     $('.back-to-top').fadeOut();
+//   }
+// };
+
+// スクロールされる度にupdateButtonを実行
+// $(window).on('scroll', updateButton);
+
+// ボタンをクリックしたらページトップにスクロールする
+$('.back-to-top').on('click', (e) => {
+  // ボタンのhrefに遷移しない
+  e.preventDefault();
+
+  // 600ミリ秒かけてトップに戻る
+  $('html, body').animate({ scrollTop: 0 }, 600);
+});
+
+// ページの途中でリロード（再読み込み）された場合でも、ボタンが表示されるようにする
+// updateButton();
+
+
+// fullPage.js
+$(document).ready(function() {
+  $('#fullpage').fullpage();
 });
