f7f1f27b19a133523aa12de91d540e62fc2ffd80
tomohiro.mizukami <water.yukuhashi@gmail.com>
2022-01-04 18:01:41 +0000

    add favicon and update animation

--- js/main.js
+++ js/main.js
@@ -30,14 +30,13 @@ $(document).ready(function() {
   'use strict';
 
   // variables
-  var $isAnimatedSecond = $('.concept .is-animated'),
-    $isAnimatedSecondSingle = $('.concept .is-animated__single'),
-    $isAnimatedThird = $('.about .is-animated'),
-    $isAnimatedThirdSingle = $('.about .is-animated__single'),
-    $isAnimatedFourth = $('.works .is-animated'),
-    $isAnimatedFourthSingle = $('.works .is-animated__single'),
-    $isAnimatedFifth = $('.contact .is-animated'),
-    $isAnimatedFifthSingle = $('.contact .is-animated__single');
+  var $conceptZoomIn = $('.concept .zoom-in'),
+    $conceptFadeInUp = $('.concept .fade-in-up'),
+    $aboutZoomIn = $('.about .zoom-in'),
+    $aboutFadeInUp = $('.about .fade-in-up'),
+    $works = $('.works-area'),
+    $contactForm = $('.contact .form-list'),
+    $contactBtn = $('.contact .submit-btn');
 
   // initialize fullPage
   $('#fullpage').fullpage({
@@ -57,13 +56,10 @@ $(document).ready(function() {
        * from the first section to another sections 
        */
 
-      // first animation
+      // Concept animation
       if (index == 1 && nextIndex == 2) {
-        $isAnimatedSecond.addClass('animate__animated animate__fadeInUp animate__delay-1s');
-        $isAnimatedSecond.eq(0).css('--animate-delay', '.6s');
-        $isAnimatedSecond.eq(1).css('--animate-delay', '.9s');
-        $isAnimatedSecond.eq(2).css('--animate-delay', '1.2s');
-        $isAnimatedSecondSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        $conceptZoomIn.addClass('animate__animated animate__zoomIn');
+        $conceptFadeInUp.addClass('animate__animated  animate__fadeInUp');
       }
 
       /**
@@ -76,13 +72,10 @@ $(document).ready(function() {
        * when you jump (using the dot navigation) from the first section to the third one 
        */
 
-      // second animation
+      // About animation
       else if ((index == 1 || index == 2) && nextIndex == 3) {
-        $isAnimatedThird.eq(0).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '.9s');
-        $isAnimatedThird.eq(1).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '1.2s');
-        $isAnimatedThird.eq(2).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '1.5s');
-        $isAnimatedThirdSingle.eq(0).addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
-        $isAnimatedThirdSingle.eq(1).addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.6s');
+        $aboutZoomIn.addClass('animate__animated animate__zoomIn');
+        $aboutFadeInUp.addClass('animate__animated  animate__fadeInUp');
       }
 
 
@@ -97,19 +90,20 @@ $(document).ready(function() {
        * from the first or second section to the fourth one 
        */
 
-      // third animation
+      // Works animation
       else if ((index == 1 || index == 2 || index == 3) && nextIndex == 4) {
-        // $isAnimatedFourth.addClass('animated zoomIn').css('animation-delay', '.6s');
-        $isAnimatedFourthSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        $works.addClass('animate__animated animate__zoomIn animate__delay-2s');
+
         // $isAnimatedFourthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         //   $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
         // });
       }
 
-      // fourth animation
+      // Contact animation
       else if ((index == 1 || index == 2 || index == 3 || index == 4) && nextIndex == 5) {
-        $isAnimatedFifth.addClass('animate__animated animate__fadeInUpBig animate__delay-1s').css('--animate-delay', '.6s');
-        $isAnimatedFifthSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        $contactForm.addClass('animate__animated animate__zoomIn animate__delay-1s');
+        $contactBtn.addClass('animate__animated  animate__fadeInUpBig animate__delay-2s');
+        
         // $isAnimatedFifthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
         //   $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
         // });
