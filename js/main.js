301a29d1f1eabe8fa08796e4f121eef2a6d8bfd9
tomohiro.mizukami <water.yukuhashi@gmail.com>
2022-01-04 05:30:42 +0000

    kadai

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $("#g-nav a, #header a").click(() => { //ナビゲーションのリンクがク
 // slick
 $(document).ready(function() {
   $('.carousel').slick({
-    // autoplay: true, // 自動再生
+    autoplay: true, // 自動再生
     autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
     arrows: false, // 左右のナビゲーションを無効にする
     dots: true, // スライド下にドットを表示する
@@ -24,42 +24,98 @@ $(document).ready(function() {
 });
 
 
-// Waypoints + Animate.css
-const effects = ['fadeInUp', 'zoomIn'];
-// ${effect}クラスの付いた要素にwaypointを登録
-effects.forEach((effect) => {
-  // ${effect}クラスの付いた要素にwaypointを登録
-  $(`.${effect}`).waypoint({
-    handler(direction) {
-      // 要素が画面上に来るとここが実行される
-      if (direction === 'down') {
-        /**
-         * 下方向のスクロール
-         * イベント発生元の要素に${effect}クラスを付けることで
-         * アニメーションを開始
-         */
-        $(this.element)
-          .addClass(`animate__${effect}`)
-          .css('opacity', '1')
-          .removeClass(`${effect}`);
+// fullPage.js + Animate.css
+(function($) {
 
-        /**
-         * waypointを削除することで、この要素に対しては
-         * これ以降handlerが呼ばれなくなる
-         */
-        this.destroy();
-      }
-    },
+  'use strict';
 
-    // 要素が画面上に来たらhandlerを実行
-    offset: '100%',
-  });
-});
+  // variables
+  var $isAnimatedSecond = $('.concept .is-animated'),
+    $isAnimatedSecondSingle = $('.concept .is-animated__single'),
+    $isAnimatedThird = $('.about .is-animated'),
+    $isAnimatedThirdSingle = $('.about .is-animated__single'),
+    $isAnimatedFourth = $('.works .is-animated'),
+    $isAnimatedFourthSingle = $('.works .is-animated__single'),
+    $isAnimatedFifth = $('.contact .is-animated'),
+    $isAnimatedFifthSingle = $('.contact .is-animated__single');
 
-// fullPage.js
-$(document).ready(function() {
+  // initialize fullPage
   $('#fullpage').fullpage({
-    //Navigation
-    anchors: ['hero', 'concept', 'about', 'works', 'contact']
+
+    navigation: true,
+    anchors: ['hero', 'concept', 'about', 'works', 'contact'],
+    onLeave: function(index, nextIndex, direction) {
+
+      /**
+       * use the following condition: 
+       *
+       *   if( index == 1 && direction == 'down' ) {
+       *
+       * if you haven't enabled the dot navigation
+       * or you aren't interested in the animations that occur 
+       * when you jump (using the dot navigation) 
+       * from the first section to another sections 
+       */
+
+      // first animation
+      if (index == 1 && nextIndex == 2) {
+        $isAnimatedSecond.addClass('animate__animated animate__fadeInUp animate__delay-1s');
+        $isAnimatedSecond.eq(0).css('--animate-delay', '.6s');
+        $isAnimatedSecond.eq(1).css('--animate-delay', '.9s');
+        $isAnimatedSecond.eq(2).css('--animate-delay', '1.2s');
+        $isAnimatedSecondSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+      }
+
+      /**
+       * use the following condition: 
+       *
+       *   else if( index == 2 && direction == 'down' ) {
+       *
+       * if you haven't enabled the dot navigation
+       * or you aren't interested in the animations that occur 
+       * when you jump (using the dot navigation) from the first section to the third one 
+       */
+
+      // second animation
+      else if ((index == 1 || index == 2) && nextIndex == 3) {
+        $isAnimatedThird.eq(0).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '.9s');
+        $isAnimatedThird.eq(1).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '1.2s');
+        $isAnimatedThird.eq(2).addClass('animate__animated animate__fadeInUp animate__delay-1s').css('--animate-delay', '1.5s');
+        $isAnimatedThirdSingle.eq(0).addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        $isAnimatedThirdSingle.eq(1).addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.6s');
+      }
+
+
+      /**
+       * use the following condition:
+       *
+       *   else if( index == 3 && direction == 'down' ) {
+       *
+       * if you haven't enabled the dot navigation
+       * or you aren't interested in the animations that occur 
+       * when you jump (using the dot navigation) 
+       * from the first or second section to the fourth one 
+       */
+
+      // third animation
+      else if ((index == 1 || index == 2 || index == 3) && nextIndex == 4) {
+        // $isAnimatedFourth.addClass('animated zoomIn').css('animation-delay', '.6s');
+        $isAnimatedFourthSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        // $isAnimatedFourthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        //   $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+        // });
+      }
+
+      // fourth animation
+      else if ((index == 1 || index == 2 || index == 3 || index == 4) && nextIndex == 5) {
+        $isAnimatedFifth.addClass('animate__animated animate__fadeInUpBig animate__delay-1s').css('--animate-delay', '.6s');
+        $isAnimatedFifthSingle.addClass('animate__animated animate__zoomIn animate__delay-1s').css('--animate-delay', '.3s');
+        // $isAnimatedFifthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        //   $(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+        // });
+      }
+    }
+
   });
-});
+
+})(jQuery);
